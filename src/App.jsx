f18f4c3c24de36411d4437b9a6b9538dc7bb131d
andyhdang <andyhd0614@gmail.com>
2024-09-17 10:46:59 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import './App.css'
+import Button from './components/Button/Button'
+import AddIcon from "@mui/icons-material/Add";
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+
+
+function App() {
+
+  return (
+    <>
+        <h1>My Component Library</h1>
+        <h2>Buttons</h2>
+        <h3>Examples</h3>
+        <h4>Types</h4>
+        <p className='code'>default, primary, subtle, dashed, link</p>
+        <div className="examples-wrapper">
+
+          <Button
+            type='default'
+            label='Default Button'
+          />
+          <Button
+            type='primary'
+            label='Primary Button'
+          />
+          <Button
+            type='subtle'
+            label='Subtle Button'
+          />
+          <Button
+            type='dashed'
+            label='Dashed Button'
+          />
+          <Button
+            type='link'
+            label='Link Button'
+          />
+
+        </div>
+        <h4>Sizes</h4>
+        <p className='code'>small, default, large</p>
+
+        <div className="examples-wrapper">
+
+          <Button
+            size='small'
+            label='Small Button'
+          />
+          <Button
+            label='Default Size Button'
+          />
+          <Button
+            size='large'
+            label='Large Button'
+          />
+
+        </div>
+        <h4>Icons</h4>
+        <p className='code'>leadingIcon, trailingIcon</p>
+        <div className="examples-wrapper">
+          <Button
+            leadingIcon={<AddIcon></AddIcon>}
+            label='Button with leading icon'
+          />
+          <Button
+            trailingIcon={<CloseRoundedIcon></CloseRoundedIcon>}
+            label='Button with trailing icon'
+          />
+
+        </div>
+        <h4>isDisabled</h4>
+        <p className='code'>true, false</p>
+        <div className="examples-wrapper">
+          <Button
+            label='Disabled Button'
+            isDisabled={true}
+          />
+          <Button
+            label='Enabled Button'
+            isDisabled={false}
+          />
+          </div>
+
+          <h4>isFullWidth</h4>
+          <p className='code'>true, false</p>
+          <div className="examples-wrapper">
+            <Button
+              label='Full Width Button'
+              isFullWidth={true}
+            />
+            <Button
+              label='Normal Button'
+              isFullWidth={false}
+            />
+          </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = ({size='', label='Button', onClick, type='default', shape='rounded', leadingIcon, trailingIcon, isFullWidth, isDisabled}) => {
+    return (
+        <button className={`${size} ${type} ${shape} ${isFullWidth? 'full-width': ''} ${isDisabled? 'disabled':''}`} onClick={onClick}>
+            {leadingIcon}{label}{trailingIcon}
+        </button>
+    );
+};
+
+Button.propTypes = {
+    size: PropTypes.oneOf(['small', 'large']),
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf(['default', 'primary', 'subtle', 'dashed', 'text', 'link']),
+    leadingIcon: PropTypes.element,
+    trailingIcon: PropTypes.element,
+    isFullWidth: PropTypes.bool,
+    shape: PropTypes.oneOf(['rounded', 'circular']),
+    isDisabled: PropTypes.bool,
+};
+
+export default Button;
