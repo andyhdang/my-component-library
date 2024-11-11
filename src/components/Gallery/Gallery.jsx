cf689ae028e4fa9a8edb47b1436a7c0652febeec
andyhdang <andyhd0614@gmail.com>
2024-11-11 12:09:30 -0800

    created gallery component

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Gallery.css';
+
+const Gallery = () => {
+    return (
+        <div>
+            {/* Your gallery content goes here */}
+        </div>
+    );
+};
+
+export default Gallery;
